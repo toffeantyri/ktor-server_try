77633aa86e01eb12f41b3a3a7c32935b80ee1412
toffeantyri <toffeantyri@gmail.com>
2025-05-04 13:09:19 +0500

    add simple Routing.kt with RoutingRoot

--- src/main/kotlin/Routing.kt
+++ src/main/kotlin/Routing.kt
@@ -1,10 +1,20 @@
 package ru.antoffy.backend
 
+import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 
 fun Application.configureRouting() {
+
+    install(RoutingRoot) {
+        route("/", HttpMethod.Get) {
+            handle {
+                call.respondText("Hello World 2!")
+            }
+        }
+    }
+
     routing {
         get("/") {
             call.respondText("Hello World!")
